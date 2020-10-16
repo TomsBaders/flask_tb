389a630cb2df918ad08f424006901f615f3940d8
[Students] Toms Baders <93DPTBaders@rvt.lv>
2020-10-16 16:15:19 +0300

    Third Commit

--- main.py
+++ main.py
@@ -4,11 +4,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("About.html")
+    return render_template("home.html")
 
 @app.route("/about")
 def about():
     return render_template("Contact.html")
+
+@app.route("/test")
+def test():
+    return render_template("Test.html")
 
 
 app.run(host="0.0.0.0", port=80, debug=True)
